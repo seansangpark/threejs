5c6053438c750676a30d838595f28e4a01f4d0dd
Sean Park <seansangpark@gmail.com>
2023-07-31 17:40:03 -0400

    custombutton created and styled

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -45,7 +45,12 @@ const Home = () => {
                 customization tool. <strong>Unleash your imagination</strong>{' '}
                 and define your own style.
               </p>
-              <CustomButton />
+              <CustomButton
+                type='filled'
+                title='Customize It'
+                handleClick={() => (state.intro = false)}
+                customStyles='w-fit px-4 py-2.5 font-bold text-sm'
+              />
             </motion.div>
           </motion.div>
         </motion.section>
