bc66fc8c8c069adc11e14ff3422400e975a86b8c
Sean Park <seansangpark@gmail.com>
2023-08-04 20:41:11 -0400

    new route created

--- server/index.js
+++ server/index.js
@@ -2,11 +2,14 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
+import dalleRouters from './routes/dalle.routes.js';
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+app.use('/api/v1/dalle', dalleRouters);
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello from DALL.E' });
