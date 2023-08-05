62e248a0192d1d36b815574dc1b881e759cf3121
Sean Park <seansangpark@gmail.com>
2023-08-04 21:02:21 -0400

    moved gitignore

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -59,7 +59,21 @@ const Customizer = () => {
     if (!prompt) return alert('Please enter a prompt');
 
     try {
-      // call our backend to generate an AI image!
+      setGeneratingImg(true);
+
+      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        method: 'POST',
+        header: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          prompt,
+        }),
+      });
+
+      const data = await response.json();
+
+      handleDecals(type, `data:image/png;base64,${data.photo}`);
     } catch (error) {
       alert(error);
     } finally {
